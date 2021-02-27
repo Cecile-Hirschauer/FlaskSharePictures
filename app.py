8b07c93a4c3f87dd5e78b84a8940ed738459cfac
chirsch <cecile.h.75@gmail.com>
2021-02-27 12:11:54 +0100

    1st steps

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, g, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import sqlite3
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+DATABASE = "app.db"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    pictures = db.execute("""SELECT path FROM pictures 
+                          ORDER BY creation_date DESC""")
+    return render_template('index.html', all_pictures=pictures)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route("/upload", methods=["POST"])
+def new_picture():
+    if 'file' not in request.files:
+        return redirect("/upload")
+    file = request.files['file']
+    # print(file)
+    if file.filename != '':
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename)) 
+        db = get_db()
+        db.execute("INSERT INTO pictures(path) VALUES (?)", [filename])
+        db.commit()
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+
+DATABASE = 'app.db'
+db = sqlite3.connect(DATABASE)
+
+cursor = db.cursor()
+
+# Creation of table "users".
+# If it existed already, we delete the table and create a new one
+cursor.execute('DROP TABLE IF EXISTS users')
+cursor.execute(""" CREATE TABLE users
+               (id INTEGER PRIMARY KEY AUTOINCREMENT,
+               login VARCHAR(250) NOT NULL,
+               password VARCHAR(250) NOT NULL)""")
+
+# Creation of table "categories".
+# If it existed already, we delete the table and create a new one
+cursor.execute('DROP TABLE IF EXISTS categories')
+cursor.execute("""CREATE TABLE categories
+               (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(200) NOT NULL)""")
+
+# We seed the table with initial values.
+# Here we insert 3 categories: "Videogames", "Boardgames" and "Outdoors"
+for name in ["Sport", "Artistique", "Gaming", "Animé", "Film"]:
+    cursor.execute("INSERT INTO categories (name) VALUES (?)", (name,))
+
+# Creation of table "pictures"
+cursor.execute("DROP TABLE IF EXISTS pictures")
+cursor.execute("""CREATE TABLE pictures
+               (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                path VARCHAR(250),
+                title VARCHAR(200),
+                description VARCHAR(200),
+                creation_date datetime default (datetime(current_timestamp)),
+                category_id INTEGER,
+                user_id INTEGER,
+                CONSTRAINT fk_categories
+                FOREIGN KEY (category_id)
+                REFERENCES categories(category_id),
+                CONSTRAINT fk_users
+                FOREIGN KEY (user_id)
+                REFERENCES users(user_id))""")
+
+# creation table "comments"
+cursor.execute("DROP TABLE IF EXISTS comments")
+cursor.execute("""CREATE TABLE comments(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                comment VARCHAR(255),
+                picture_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                date_published datetime default (datetime(current_timestamp)),
+                CONSTRAINT fk_pictures
+                FOREIGN KEY (picture_id)
+                REFERENCES pictures(picture_id),
+                CONSTRAINT fk_users
+                FOREIGN KEY (user_id)
+                REFERENCES users(user_id))""")
+
+
+
+# We save our changes into the database file
+db.commit()
+
+# We close the connection to the database
+db.close()
